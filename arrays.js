eed3b2960846200d8e3f90207813009ffd31fe79
Daniel <lopezdana@gmail.com>
2023-11-24 16:38:03 -0300

    agregado de comentarios en archivo array en fabrica

--- arrays.js
+++ arrays.js
@@ -1,7 +1,10 @@
-[] //estructura de una Array
+[] //corchetes: estructura de una Array, donde guardar valores de numeros, de string, objetos
 
-//generando un Array: le damos de nombre frutas
+//generando un Array: le damos de nombre: frutas
 var frutas = ["Manzana", "Platano", "Cereza", "Fresa"];
-console.log(frutas)
-console.log(frutas.length) //imprime 4, que es la longitud del Array
-console.log(frutas[0]) //imprime "Manzana", que es el elemento que se pidio saber en la ubicacio/indice cero
+console.log(frutas) //imprime lo que tiene el array Frutas
+console.log(frutas.length) //imprime la longitud que tiene el array Frutas: 4, con la propiedad ".length" nos dice la longitud del array.
+console.log(frutas[0]) //imprime el elemento de la posicion cero: "Manzana", porque en el indice del array frutas esta posicionado en cero el elemento Manzana...  se usa para averiguar cual es el elemento que esta en este caso en la posicion cero.
+
+// Metodos de un Array: nos van a permitir mutar elementos del Array
+var masFrutas = frutas.push("uva") //Con el Metodo ".push" va a añadir un elemento al final del array
